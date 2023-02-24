b4cdf354db53784d69ffc1ee79f7fd301de4dcf3
Romulo Americo <116516646+Romulo-Americo@users.noreply.github.com>
2023-02-24 10:31:11 -0400

    Add files via upload

---
+++ Estudo de POO/exercitando_python.py
@@ -0,0 +1,16 @@
+from uteis import Pessoa
+
+p1 = Pessoa('Luiz', 26)
+p2 = Pessoa('João', 32)
+p1.comer('maçã')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.parar_falar()
+p1.comer('alimento')
+
+
+
+
+
+ 
